4959a053dee5f62185d70c573d0b1b795c61d77f
Sneikder1 <sneikdermendez@gmail.com>
2022-09-22 23:53:13 -0500

    splots

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,86 @@
-<script>
-  export default{
-    data: () => ({
-      contador : 10,
-    }),
-    methods: {
-      saludar(){
-        console.log("Hola 10A")
-      },
-      incrementar(){
-        this.contador++;
-      },
-      decrementar(){
-        this.contador--;
-      },
-      resetear(){
-        this.contador=10;
-      }
-    }
-  };
-</script>
-
 <template>
-  <div class="window-notice" id="window-notice">
-    <div class="content">
-        <div class="content-text">SERGIO MARTINEZ MENDEZ 10A
-        
-    </div>
-</div>
-</div>
-  <img src ="./assets/logo.svg" alt="logo VUE"
-  width="135" heigth="135">
-  <div class="container">
-    
-    <h4>Sergio Martinez Mendez 10A</h4>
-    <h1>Hola desde VUE.js</h1>
-  <hr>
-  <h2>Contador {{contador}}</h2>
-  <button @click="incrementar()">+1</button>
+  <header>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   
-  <button @click="decrementar()">-1</button>
-  <button @click="resetear()">Reset</button>
+    
+      <ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Nuestros Platos</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Mi cuenta</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Blog </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+  <h3>Sergio Martinez 10 A</h3>
+</ul>
+</header>
+
+<div class="card text-center">
+  <div class="card-header">
+    <h5>Comida rapida</h5> 
   </div>
+  <div class="card-body">
+    
+    <a href="#" class="btn btn-primary">Ensaladas</a>
+    <a href="#" class="btn btn-primary">Pizzas</a>
+    <a href="#" class="btn btn-primary">Hamburguesas</a>
+    
+  </div>
+  <div class="card-footer text-muted">
+    Pide ahora.
+  </div>
+</div>
+  <hr/>
+  <card-vue url="https://www.pequerecetas.com/wp-content/uploads/2020/07/ensalada-cobb-receta.jpg" width="8rem"
+        height="8rem" nombre="Ensalada"/>
+  <card-vue url="https://www.saborusa.com/wp-content/uploads/2019/12/origen-de-la-pizza-1.jpg" nombre="Pizza"/>
+  <card-vue url="https://cdn.colombia.com/gastronomia/2011/08/25/pizza-margarita-3684.jpg" nombre="Pizzas"/>
+  <card-vue url="https://d320djwtwnl5uo.cloudfront.net/recetas/share/pizza_Mh3H4eanyBKEsStv1YclPWTf9OUqIi.png" nombre="Pizzas"/>
+  <card-vue url="https://images.hola.com/imagenes/cocina/noticiaslibros/20210528190401/dia-internacional-hamburguesa-recetas-2021/0-957-454/dia-hamburguesa-m.jpg" nombre="Hamburgesa"/>
+  <card-vue url="https://www.recetasnestle.com.ec/sites/default/files/srh_recipes/4e4293857c03d819e4ae51de1e86d66a.jpg" nombre="Hamburgesas"/>
+  <div class="footer">
+  <p>Derechos de autor 2022©
+
+Encuentranos en el Centro de la ciudad de tehuacan</p>
+</div>
 </template>
 
+<script>
+  import CardVue from './components/Card.vue';
+  export default{
+     components:{CardVue},
+  };
+</script>
 <style scoped>
-  .container{
-  text-align:center;
-  }
-  small{
-    color:#666
-  }
-  h1{
-    font-weight: bolder;
-  }
-  h2{
-    font-size: 2.5rem;
-  }
-  button{
-    cursor: pointer;
-    font-size: 1.5rem;
-    margin: 0px 8px;
-  }
-  .window-notice{
-    background: rgba(10, 12, 10, .20);
-    left: 0;
-   text-align: center;
-    right: 0;
-    top: 0;
-    display: flex;
-    position: fixed;
-    z-index: 99;
-}
 
+  ul{
+    background: #288BA8;
+
+  }
+  h3{
+    text-align: center;
+  }
+  h3{
+    text-align: right;
+    text-decoration: none;
+        color: snow;
+        
+        padding: 0.3rem;
+       
+  }
+  .footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: black;
+  color: white;
+  text-align: center;
+}
+ 
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+<template>
+    <grid>
+  <img v-bind:src="url" alt="Ensalada">
+  <h2>{{nombre}}</h2>
+  <a :href="url" target="_blank">Comprar</a>
+  <h4>20$</h4>
+    </grid>
+    
+</template>
+<script>
+import Grid from "./Grid.vue";
+    export default{
+    props: {
+        url: {
+            String,
+        },
+        nombre: {
+            String,
+        }
+    },
+    components: { Grid }
+}
+</script>
+
+<style scoped>
+    
+    img{ 
+        width: 15rem;
+        height: 10rem;
+        padding: 0.5rem;
+        background: snow;
+    }
+    a{
+        text-decoration: none;
+        color: black;
+        background-color: beige;
+        padding: 0.3rem;
+        border-radius: 5px;
+        border:1px solid #fff
+        
+    }
+</style>
+
